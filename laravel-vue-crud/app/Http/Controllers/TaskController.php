7aa4535a2f06542d9bd93dbd9a8468e90e358c87
laxsanjkr <s.navalaxsan@gmail.com>
2021-02-17 11:11:47 +0100

    Mise en place du controller et model TASK

---
+++ laravel-vue-crud/app/Http/Controllers/TaskController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all()->toArray();
+        return array_reverse($tasks);      
+    }
+
+    public function store(Request $request)
+    {
+        $task = new Task([
+            'name' => $request->input('name'),
+            'detail' => $request->input('detail')
+        ]);
+        $task->save();
+
+        return response()->json('Task created!');
+    }
+
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return response()->json($task);
+    }
+
+    public function update($id, Request $request)
+    {
+        $task = Task::find($id);
+        $task->update($request->all());
+
+        return response()->json('Task updated!');
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        return response()->json('Task deleted!');
+    }
+}
